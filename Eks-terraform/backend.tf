48b45e620d5d6f9fbca4eeb4b51851389a0bf05e
donpasscal <32339955+DONPASSCAL@users.noreply.github.com>
2023-12-25 23:46:29 -0500

    Update backend.tf

--- Eks-terraform/backend.tf
+++ Eks-terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "dohn-buckeet" # Replace with your actual S3 bucket name
-    key    = "terraform.tfstate"
+    key    = "Jenkins/terraform.tfstate"
     region = "us-east-1"
   }
 }
